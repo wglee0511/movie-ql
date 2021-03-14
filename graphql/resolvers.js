90817459c911d098f16d98c5f82bf926674ad05f
wglee0511 <ranazang@naver.com>
2021-03-14 19:57:16 +0900

    Set up graphql server

---
+++ graphql/resolvers.js
@@ -0,0 +1,7 @@
+ const resolvers = {
+    Query: {
+        name:() => console.log(`Hello`)
+    }
+}
+
+export default resolvers;
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { GraphQLServer } from 'graphql-yoga'
+import resolvers from './graphql/resolvers';
 
 
 const server = new GraphQLServer({
-
+    typeDefs: "./graphql/schema.graphql",
+    resolvers: resolvers
 });
 
 
